88764f331081217549a2b108dd2a2c38c91a0635
macbookair <macbookair@localhost>
2021-05-12 16:12:55 +0000

    Merging

--- src/bionicProInc/db/ifaces/DBManager.java
+++ src/bionicProInc/db/ifaces/DBManager.java
@@ -11,27 +11,23 @@ public interface DBManager {
 	public void disconnect();
 
 	public void addProduct(Product prod);
-
+	
 	public void removeProduct(int id);
-
+	
 	public void addCharacteristic(Characteristic ch);
-
+	
 	public List<Characteristic> getCharacteristics(int id);
 
 	public void addMaterial(Material mat);
-
+	
 	public void addCustomer(Customer cust);
 	
-<<<<<<< HEAD
-	public void addToProdCh(Product prod, Characteristic c);
-=======
 	public int getCustomerID(String email);
 
 	public void addProdIntoCh(Product prod, Characteristic ch);
->>>>>>> branch 'master' of https://github.com/aymane-boustta/bionicProInc
 
 	public void addChIntoProd(Material mat);
-
+	
 	public void addProdIntoMat(Product prod);
 
 	public void addMatIntoProd(Material mat);
@@ -42,32 +38,40 @@ public interface DBManager {
 
 	public void addEngineer(Engineer eng);
 	
-	public int getEngineerID(String email);
-
-	public List<Engineer> viewEngineersID();
-
-	public List<Integer> viewProjectAchieved(int id);
-
-	public Float viewBonus(int id);
-
 	public void addOrder(Order ord);
-
-	public void addToOrder(Product prod, Order ord);
-
-	public List<Product> viewProductsFromOrder(int id);
-
-	public List<Integer> viewOtherOrders(int id);
-
-	public List<String> viewCart(Order ord);
-
-	public void deleteProdFromCart(String name, Order ord);
 
 	public List<String> searchProductByBody(String bodypart);
 
 	public List<String> viewBodyparts();
 
-	public List<Characteristic> viewCharacteristicsFromProduct(int id);
+	public void addToOrder(Product prod, Order ord);
 
+	public List<String> viewCart(Order ord);
+	
+	public List<Product> viewProductsFromOrder(int id);
+
+	public void deleteProdFromCart(String name, Order ord);
+	
+	public List<Integer> viewOtherOrders(int id);
+	
+	public List<Characteristic> viewCharacteristicsFromProduct(int id);
+	
 	public List<Material> viewMaterialsFromProduct(int id);
+	
+	public List<Engineer> viewEngineersID();
+
+	public List<Integer> viewProjectAchieved(int id);
+	
+	public Float viewBonus(int id);
+	
+
+
+
+
+	
+
+	
+
+	
 
 }
